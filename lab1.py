3699b8078263c31811a265e7e33dcbf1517cb6d9
omarelnokrashy <omarelsayedelnokrashy@gmail.com>
2025-10-20 22:39:10 +0300

    libraries

--- lab1.py
+++ lab1.py
@@ -1,7 +1,21 @@
-import re
-password = input("Enter your password: ")
+# import re
+# password = input("Enter your password: ")
 
-if(re.search(r'[A-Z]+',password)and re.search(r'[a-z]+',password) and re.search(r'[0-9]+',password) and len(password)>=8 and re.search(r'[@$!%*#?&]+',password)):
-    print("Strong Password")
-else:
-    print("Weak Password")
+# if(re.search(r'[A-Z]+',password)and re.search(r'[a-z]+',password) and re.search(r'[0-9]+',password) and len(password)>=8 and re.search(r'[@$!%*#?&]+',password)):
+#     print("Strong Password")
+# else:
+#     print("Weak Password")
+
+from scipy.optimize import linprog
+
+obj = [-2,-5]
+lhs_ineq = [[1,2],
+             [5,3]]
+
+rhs_ineq = [16,45]
+
+bnd = [(0,float("inf")),
+       (0,float("inf"))]    
+
+opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq, bounds=bnd)
+print(opt)
